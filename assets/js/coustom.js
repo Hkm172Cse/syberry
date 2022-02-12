306ede12e2afb1060453542734a2683e189fe71e
Hkm172Cse <azizulhakim000111@gmail.com>
2022-02-12 17:08:17 +0600

    second slider is created

---
+++ assets/js/coustom.js
@@ -0,0 +1,27 @@
+const slider = document.querySelector('.slider');
+const leftArrow = document.querySelector('.left');
+const rightArrow = document.querySelector('.right');
+
+var sectionIndex = 0; 
+leftArrow.addEventListener('click', function(){
+     
+    sectionIndex = (sectionIndex > 0)? sectionIndex -1: 0;
+     slider.style.transform = "translate("+(sectionIndex)*-25+"%)";
+
+ 
+})
+
+
+rightArrow.addEventListener('click', function(){
+   // sectionIndex = (sectionIndex < 3 ) ? sectionIndex + 1: 3;
+   if(sectionIndex < 3){
+       sectionIndex  = sectionIndex +1;
+       slider.style.transform = "translate("+(sectionIndex)*-25+"%)";
+   }
+   else{
+    sectionIndex = -1;
+   }
+
+   
+   
+});
